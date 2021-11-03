463e040e9a3be37f4af4f165064cd718e30497ea
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2021-11-03 16:54:50 -0700

    updates

---
+++ src/modules/dataController/TaskOrderController.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+export default class TaskOrderController {
+
+    static sortBy(field,isAscending) {
+
+    }
+
+
+}
---
+++ src/modules/sorting/sortingAlgorithm.js
@@ -0,0 +1,88 @@
+/*
+
+@ISortData: {
+field:"name"|"time"|"priority",
+isAscending:Boolean
+}
+
+@ITask: {
+    _id:String (UUIDV4);
+    isCompleted:Boolean;
+    text:String;
+    creationDate:Number (Unix);
+    priority: Number;
+}
+ */
+
+export const NAME = "name"
+export const TIME = "time"
+export const PRIORITY = "priority"
+
+function applySorting(sortDataList,tasks) {
+    for (let i = 0; i < sortDataList.length; i++) {
+        const sortData = sortDataList[i]
+        const sortFunction = getSortFunction(sortData)
+        if (isFirstSortField(i)) {
+           sortTasks(tasks,sortFunction,sortData)
+        }
+        else {
+            const prevSortData = sortDataList[i - 1]
+            const prevSortFunction = getSortFunction(prevSortData)
+            sortBySecondaryKey(prevSortData,prevSortFunction,sortData,tasks,sortFunction)
+        }
+
+    }
+    return tasks
+}
+
+function isFirstSortField(index) {
+    return i == 0
+}
+
+function sortTasks(tasks,sortFunction,sortData) {
+    tasks.sort((a, b) => sortFunction(a, b, sortData.isAscending))
+}
+
+function sortBySecondaryKey(prevSortData,prevSortFunction,sortData,tasks,sortFunction) {
+    tasks.sort((a,b) => {
+        if (valuesAreEqual(prevSortData,prevSortFunction,a,b)) {
+            return sortFunction(a,b,sortData.isAscending)
+        }
+    })
+}
+
+
+
+function valuesAreEqual(sortData,sortFunction,a,b) {
+    return sortFunction(a,b,sortData.isAscending)
+}
+
+function getSortFunction({field,isAscending}) {
+    switch (field) {
+        case NAME : return sortByName;
+        case TIME: return sortByTime;
+        case PRIORITY: return sortByPriority;
+        default: return
+    }
+}
+
+function isFirstSortCriteria({field,isAscending}) {
+    switch (field) {
+        case "name": return sortByName
+    }
+}
+
+function sortByName(taskA,taskB,isAscending) {
+    const comparison = taskA.text.localeCompare(taskB.text)
+    return isAscending ? comparison : -1 * comparison
+}
+
+function sortByPriority(taskA,taskB,isAscending) {
+    const comparison = taskA.priority - taskB.priority
+    return isAscending ? -1 * comparison : comparison
+}
+
+function sortByTime(taskA,taskB,isAscending) {
+    const comparison = taskA.creationDate - taskB.creationDate
+    return isAscending ? comparison : -1 * comparison
+}
