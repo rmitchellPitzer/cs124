0791fb73625de2f577df6eea07d7fa319540e452
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2021-12-06 20:46:14 -0800

    main app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./css/global.css"
 import "typeface-roboto"
 import Main from "./components/Main/index"
+import Header from "./components/Header/Header";
 import { connect } from "react-redux";
 
 function App({isSignedIn}) {
-  return
-  (
-    <div className='container'>
-      <Header/>
-      { isSignedIn && <Main/> }
-      { !isSignedIn && <SignInScreen/> }
-    </div>
-  )
+  return (
+      <div className='container'>
+        <Header/>
+        { isSignedIn && <Main/> }
+        { !isSignedIn && <SignInScreen/> }
+      </div>
+      )
 }
 
 function mapToState(state) {
