e62d4fe9c64a5b323ac7c40583cf498d1355c32d
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2021-12-06 20:30:49 -0800

    updated firebase config

--- src/modules/db/firebaseConfig.js
+++ src/modules/db/firebaseConfig.js
@@ -1,8 +1,9 @@
 export default  {
-    apiKey: "AIzaSyCd9qqxvMpEKpBzwfWcc2tlRFa6ICaLH_s",
-    authDomain: "hmc-cs124-fa21-labs.firebaseapp.com",
-    projectId: "hmc-cs124-fa21-labs",
-    storageBucket: "hmc-cs124-fa21-labs.appspot.com",
-    messagingSenderId: "949410042946",
-    appId: "1:949410042946:web:0113b139a7e3cd1cc709db"
+    apiKey: "AIzaSyCX-SKr6NJLpnc0-fvxLOC8fORYMNY67AQ",
+    authDomain: "classproject124-c3fc8.firebaseapp.com",
+    projectId: "classproject124-c3fc8",
+    storageBucket: "classproject124-c3fc8.appspot.com",
+    messagingSenderId: "597806616477",
+    appId: "1:597806616477:web:0956d0b95073c73c9f6974",
+    measurementId: "G-FJ0WB34RNT"
 }
